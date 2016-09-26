3e733800678fe1d27c40bc86a9c9f28aaab263b9
aisthanestha <ghendrickson47@gmail.com>
2016-09-26 18:28:19 -0500

    Updated

--- github_recloner.py
+++ github_recloner.py
@@ -9,39 +9,42 @@ os.system("cd")
 os.system("mkdir repo")
 os.system("mv ~/repo/python/github_recloner.py ~/")
 os.system("rm -r -d ~/repo")
-print "0. python"
-print "1. bash"
-print "2. httpscreenshot"
-print "3. config"
-print "4. dvwa_installer"
+print "0. Python"
+print "1. Bash"
+print "2. Httpscreenshot"
+print "3. Config"
+print "4. Dvwa_installer"
 valid="0"
 while valid=="0":
-    choice =raw_input("what repo do you want")
+    choice =raw_input("Which repo do you want?")
     if choice == "0":
-    os.system("git clone https://github.com/aisthanestha/python")
-    valid="1"
+        os.system("git clone https://github.com/aisthanestha/python")
+        valid="1"
     elif choice == "1":
-    os.system("git clone https://github.com/aisthanestha/bash")
-    valid="1"
+        os.system("git clone https://github.com/aisthanestha/bash")
+        valid="1"
     elif choice == "2":
-    os.system("git clone https://github.com/aisthanestha/httpscreenshot")
-    valid="1"
+        os.system("git clone https://github.com/aisthanestha/httpscreenshot")
+        valid="1"
     elif choice == "3":
-    os.system("git clone https://github.com/aisthanestha/config")
-    valid="1"
+        os.system("git clone https://github.com/aisthanestha/config")
+        valid="1"
     elif choice == "4":
-    os.system("git clone https://github.com/aisthanestha/dvwa_installer")
-    valid="1"
+        os.system("git clone https://github.com/aisthanestha/dvwa_installer")
+        valid="1"
     else: print "Enter a valid choice. (0-5)"
-valid_remove=="0"
-while valid_remove="0":
+
+valid_remove="0"
+while valid_remove=="0":
         choice_1 = raw_input("Remove ~/github_recloner.py?[y/n]")
         if choice_1 == "y" or choice_1 == "yes":
-                print "removing ~/github_recloner.py"
+                print "Removing ~/github_recloner.py"
                 os.system("rm ~/github_recloner.py")
+                valid_remove="1"
         elif choice_1 == "n" or choice_1 == "no":
                 print "~/github_recloner.py remains"
-        else: print "Enter valid choice. ('yes','y', 'n', 'no')
+                valid_remove="1"
+        else: print "Enter valid choice. ('yes','y', 'n', 'no')" 
 
 print "End of script.."
 
